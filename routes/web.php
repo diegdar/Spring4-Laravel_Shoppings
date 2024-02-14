b7514ca4ee6415f954088b69a712c1c16a0e313b
Diego Chacon <diegdarch@hotmail.com>
2024-02-14 15:50:46 +0100

    creation of routes for Products and Purchases Controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\PurchaseController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', HomeController::class);
+
+Route::resource('products', ProductController::class);
+Route::resource('purchases', PurchaseController::class);
