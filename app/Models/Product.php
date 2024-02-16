d379ebac251dd63176c1f09fc36cce9bc3fd3125
Diego Chacon <diegdarch@hotmail.com>
2024-02-16 19:36:53 +0100

    creation of methods one to many and reverse in Purchase model and Product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,12 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 /*
-🗒️NOTAS:
-1: $guarded actua de una forma parecida a $fillable, pero en lugar de indicarle los campos que debe permitir guardar le indicamos el campo que debe proteger y por tanto no debe guardar si lo recibe por el formulario. 
-    ⚠️Si no tenemos un campo para proteger e igual queremos utilizar la asignacion masiva debemos dejar el array vacio.
-    
+🗒️NOTES:
+1: $guarded acts in a similar way to $fillable, but instead of indicating the fields that should be allowed to be saved, we indicate the field that should be protected and therefore should not be saved if it is received through the form.
+     ⚠️If we do not have a field to protect and we still want to use mass assignment, we must leave the array empty.    
+2: Here we set the relation: $this(Product) can only belongs to a Purchase.
 */
 
 class Product extends Model
@@ -20,4 +21,8 @@ class Product extends Model
 
     protected $guarded = [];//note 1
 
+    public function purchase(): BelongsTo
+    {
+        return $this->belongsTo(Purchase::class);//note 2
+    }
 }
